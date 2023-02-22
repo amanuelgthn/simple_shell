b0ec61d34b0aa863c1ad39905b8e6df5fd3b9bbc
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-02-22 19:44:40 +0800

    Update shell_env.c

--- shell_env.c
+++ shell_env.c
@@ -1,26 +1,25 @@
+include"shell.h"
 /**
 **shell_exit - exits the shell
 **Return: void
 **/
-
-	int shell_exit(void)
-	{
-		return (-1);
-	}
-
-
+int shell_exit(void)
+{
+	return (-1);
+}
 /**
 **shell_env - prints environment
 **Return: void
 **/
 int shell_env(void)
 {
-unsigned int i = 0;
-		while (environ[i] != NULL)
-		{
-			write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
-			write(STDOUT_FILENO, "\n", 1);
-			i++;
-		}
-		return (0);
+	unsigned int i = 0;
+	
+	while (environ[i] != NULL)
+	{
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
 	}
+	return (0);
+}
